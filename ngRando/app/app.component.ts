2b700a28cce0b077e55e4793a03cedc78ac657d5
douviz <31436354+douviz89@users.noreply.github.com>
2017-10-19 18:22:26 +0200

    Update app.component.ts

--- ngRando/app/app.component.ts
+++ ngRando/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App edited</h1>'
+    template: '<h1>My First Angular App from github</h1>'
 })
 export class AppComponent { }
  
