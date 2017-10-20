cfce563efe6df753050931d69c0d6760a98550ca
douviz <hocem.zemouri-extern@renault.com>
2017-10-20 10:29:54 +0200

    test commit from work

--- ngRando/app/app.component.ts
+++ ngRando/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App from github</h1>'
+    template: '<h1>My First Angular App from github, edited from work</h1>'
 })
 export class AppComponent { }
  
